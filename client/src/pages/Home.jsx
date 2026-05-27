7e238d8fe8b9808af427edd0a546963fb1a98e3d
uzumakiyashwanth <krishindia1952004@gmail.com>
2026-05-27 21:28:20 +0530

    fixed render backend deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 
 import { motion } from "framer-motion";
 
-const socket = io("https://hivemind-1.onrender.com");
+const socket = io("https://hivemind-5sav.onrender.com");
 
 function Home() {
 
